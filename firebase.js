858481c09420ffd94e97d2a4f89183d63f61c5c9
Fabo <randommusicd@gmail.com>
2021-09-16 16:51:31 -0500

    Primer Commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1UBDNzR-mr_K0dVrponWxdMcltawKip0",
+    authDomain: "signal-clone-5c4b5.firebaseapp.com",
+    projectId: "signal-clone-5c4b5",
+    storageBucket: "signal-clone-5c4b5.appspot.com",
+    messagingSenderId: "559839084053",
+    appId: "1:559839084053:web:20c3254bb9122dedb2581c"
+  };
