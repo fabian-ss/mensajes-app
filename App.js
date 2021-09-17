d29a4995053db45e3aad8c6dc6d43097e8774025
fabian-ss <randommusicd@gmail.com>
2021-09-17 02:44:59 -0500

    Frontend con React Native, pre-auth de firebase

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+    headerTitleAlign: 'center',
+    headerTitleStyle: {color:"white"},
+    headerStyle: {backgroundColor: "#2C6BED"},
+    headerTinColor: "white"
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Iniciar Sesión" component={LoginScreen} />
+        <Stack.Screen name="Registro"  component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
